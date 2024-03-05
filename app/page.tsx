d34ab3c176ed3cd714d7938f690db81757e1b7c3
Mausam Tewatia <60027647+MausamTewatia@users.noreply.github.com>
2024-03-06 00:43:16 +0530

    added a title

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Chat from "./assistant";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl">Chef AI</h1>
       <Chat />
     </main>
   );
