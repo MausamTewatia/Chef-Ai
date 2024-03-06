b32fba527f12cb9ca8b08188ed4ce7c067f98312
Mausam Tewatia <60027647+MausamTewatia@users.noreply.github.com>
2024-03-06 22:40:42 +0530

    New margin added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Chat from "./assistant";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center  pt-24">
+    <main className="flex min-h-screen flex-col items-center  pt-24 px-8">
       <h1 className="text-2xl p-8">Chef AI</h1>
       <Chat />
     </main>
